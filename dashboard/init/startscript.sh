bfd41c40708c1656a976ab63aba00f92a98f2940
jyotirmoyc <jyotirmoyc@outlook.in>
2021-05-18 08:45:13 +0530

    dashboard windows updates

--- dashboard/init/startscript.sh
+++ dashboard/init/startscript.sh
@@ -3,6 +3,6 @@ service apache2 restart
 #cp -r /root/files/* /web/html/
 #cp /root/files/index.html /web/html/index.html
 #cp /root/files/setup-nextcloud.php /web/html/setup-nextcloud.php
-chown -R www-data:www-data /web
+#chown -R www-data:www-data /web
 echo "Running an useless command to keep container in fg"
 tail -f /dev/null
